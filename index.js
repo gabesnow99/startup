a5854552b0ec9938784bd810c777b32929b626b5
Ghee Buttersnaps <gabesnow99@penguin.lxd>
2023-04-18 16:08:56 -0600

    added the peerProxy bit

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcrypt');
 const express = require('express');
 const app = express();
 const DB = require('./database.js');
+const { PeerProxy } = require('./peerProxy.js');
 
 const authCookieName = 'token';
 
@@ -117,3 +118,5 @@ function setAuthCookie(res, authToken) {
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+new PeerProxy(httpService);
